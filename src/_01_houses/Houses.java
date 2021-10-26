9d18a62d84377bc85787c71148411b8a9d521746
root <root@6a774435ff28>
2021-10-26 01:28:33 +0000

    houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,6 +1,7 @@
 package _01_houses;
 
 import java.awt.Color;
+import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
 
@@ -9,7 +10,7 @@ public class Houses {
 	public void run() {
 	house.setSpeed(100);
 		for (int i = 0; i < 10; i++) {
-			drawHouse();
+			drawHouse("mixedBag");
 		}
 		
 		
@@ -39,8 +40,91 @@ public class Houses {
 		
 		
 		
-		
-		
 	}
+	
+	
+	
+	
+	
+	
+	public void drawHouse (String height) {
+		
+		Random size = new Random();
+		
+		
+		
+		
+		
+		int wallHeight = 0 ;
+		if (height.equals ("small")) {
+			wallHeight = 60+size.nextInt(31) ;
+		}
+		if (height.equals ("medium")) {
+			wallHeight = 120+size.nextInt(31);
+		}
+		if (height.equals ("large")) {
+			wallHeight = 250+size.nextInt(31);
+		}
+		if (height.equals ("mixedBag")) {
+			wallHeight = size.nextInt(250);
+		}
+		
+		
+		house.penDown();
+		house.setRandomPenColor();
+		house.move(wallHeight);
+		house.turn(90);
+		house.move(50);
+		house.turn(90);
+		house.move(wallHeight);
+		house.turn(270);
+		house.setPenColor(Color.green);
+		house.move(50);
+		house.turn(270);
+	}
+public void drawHouse (String height, Color color) {
+		
+		Random size = new Random();
+		
+		
+		
+		
+		
+		int wallHeight = 0 ;
+		if (height.equals ("small")) {
+			wallHeight = 60+size.nextInt(31) ;
+		}
+		if (height.equals ("medium")) {
+			wallHeight = 120+size.nextInt(31);
+		}
+		if (height.equals ("large")) {
+			wallHeight = 250+size.nextInt(31);
+		}
+		if (height.equals ("mixedBag")) {
+			wallHeight = size.nextInt(250);
+		}
+		
+		
+		house.penDown();
+		house.setPenColor(color);
+		house.move(wallHeight);
+		house.turn(90);
+		house.move(50);
+		house.turn(90);
+		house.move(wallHeight);
+		house.turn(270);
+		house.setPenColor(Color.green);
+		house.move(50);
+		house.turn(270);
+	}
+		
+		
+		
+	
+	
+	
+	
+	
+	
 	}
 
